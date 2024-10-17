2f3e007af916e6fcc049e992b19c1eb594ad6918
Mahmood Ahmad <mahmood073@gmail.com>
2024-10-17 23:49:59 +0300

    Handle the Order "saving" event

--- core/Sale/Observers/OrderObserver.php
+++ core/Sale/Observers/OrderObserver.php
@@ -3,9 +3,36 @@
 namespace Core\Sale\Observers;
 
 use Core\Sale\Models\Order;
+use Core\Inventory\Models\Product;
 
 class OrderObserver
 {
+    /**
+     * Handle the Order "saving" event.
+     */
+    public function saving(Order $order): void
+    {
+        if (request()->items) {
+            $newItems = [];
+            $order->items()->delete();
+            $order->subtotal = 0;
+            $order->tax      = 0;
+            $order->total    = 0;
+
+            foreach (request()->items as $item) {
+                $product          = Product::find($item['product_id']);
+                $order->subtotal += $product->price * $item['quantity'];
+                $newItems[] = array_merge($item, [
+                    'price' => $product->price,
+                ]);
+            }
+
+            $order->items()->createMany($newItems);
+            $order->tax   = ($order->subtotal * config('core_sale.tax')) / 100;
+            $order->total = $order->subtotal + $order->tax;
+        }
+    }
+
     /**
      * Handle the Order "created" event.
      */
