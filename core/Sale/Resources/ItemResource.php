55bf143378c3a46cc0c3fb07bd8e77081b08e070
Mahmood Ahmad <mahmood073@gmail.com>
2024-10-18 10:55:40 +0300

    showing the product details in order details

--- core/Sale/Resources/ItemResource.php
+++ core/Sale/Resources/ItemResource.php
@@ -15,12 +15,15 @@ class ItemResource extends Resource
      */
     public function toArray($request)
     {
+        $route_name = $request->route()->getName();
         return [
             'id'    => $this->id,
             'price' => $this->price,
-            $this->mergeWhen($request->route()->getName() == 'api.v1.items.show', [
-                'product' => new ProductResource($this->product),
+            $this->mergeWhen($route_name == 'api.v1.items.show', [
                 'order'   => $this->when($this->itemable_type == 'orders', new OrderResource($this->itemable)),
+            ]),
+            $this->mergeWhen(in_array($route_name, ['api.v1.items.show', 'api.v1.orders.show']), [
+                'product' => new ProductResource($this->product)
             ])
         ];
     }
